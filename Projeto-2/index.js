e7352a76e91bc944fa39822c85493c53c6cf38d3
Felipe Muniz <felipemuniz93@hotmail.com>
2023-10-15 12:42:22 -0300

    ex node js

---
+++ Projeto-2/index.js
@@ -0,0 +1,10 @@
+
+var x = 1.5;
+var xpto = 0 + (20 * 90 * 75 - 4 + x * ( 20 / 90 /75 + 4 + x * x) ** (20 / 90 / 75 + 4 + x * x) * (2 + 8 * (20 - 1) * - 1)* -1) + 1;
+
+
+
+console.log("Felipe Muniz");
+console.info("Felipe Muniz");
+console.error("Felipe Muniz");
+console.warn(xpto);
---
+++ README.md
@@ -0,0 +1,1 @@
+# FaculdadeFlamingo-PortaldasFormasNodejs
